64982f9760db864454a43b19830e1a3df58a0ade
PiiaJ <65115924+PiiaJ@users.noreply.github.com>
2021-11-19 11:18:50 +0200

    Update index.php
    Lisätty countdown ja progress bar hahmotelmat.

--- index.php
+++ index.php
@@ -25,6 +25,43 @@
 			<br>
 			<br>
 			
+			<!-- Esimerkki aikalaskurista (countdown)
+
+			// Set the date we're counting down to, meidän tapauksessa timestamp + 1 tunti
+			var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+			// Update the count down every 1 second
+			var x = setInterval(function() {
+
+  			// Get today's date and time
+  			var now = new Date().getTime();
+
+  			// Find the distance between now and the count down date
+  			var distance = countDownDate - now;
+
+  			// Time calculations for hours, minutes and seconds
+  			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  			// Display the result in the element with id="demo"
+  			document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  			+ minutes + "m " + seconds + "s ";
+
+  			// If the count down is finished, write some text, meidän tapauksessa käynnistyisi checkResults()-funktio
+  			if (distance < 0) {
+    				clearInterval(x);
+    				document.getElementById("demo").innerHTML = "EXPIRED";
+ 			}
+			}, 1000);
+
+			-->
+			
+			<!-- Progress bar, jos halutaan laittaa. Value +1 jokaisesta vastauksesta. Tehtäviä yht. 57.
+			<label for="file">Your progress:</label>
+			<progress id="file" value="0" max="57"> 0 % </progress>
+			-->
+			
 			<input type="submit" value="Start exam!" name="start" onclick="start()">
 			
 			<h2>Basic Calculations 10 Points</h2><br>
